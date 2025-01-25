590fcbe626efea69610e1dab2cf4904c5fd01a65
Vaibhav Gajendra Adhav <154625347+vaibhavadhav84@users.noreply.github.com>
2025-01-25 21:49:25 +0530

    Update CatTable.js

--- src/components/CatTable.js
+++ src/components/CatTable.js
@@ -1,23 +1,30 @@
 import React from "react";
 
 const CatTable = ({ catData }) => {
+  
   return (
     <div className="table-container">
       <table>
         <thead>
           <tr>
-            <th>Image</th>
-            <th>Breed</th>
+            <th>ID</th>
+            <th>Breed Name</th>
             <th>Origin</th>
             <th>Temperament</th>
+            <th>Description</th>
           </tr>
         </thead>
         <tbody>
+
+        <img src={catData.url} alt="Cat" className="cat-image" />
+        
           <tr>
-            <td><img src={catData.url} alt="Cat" className="cat-image" /></td>
+
+            <td>{catData.breeds ? catData.breeds[0].id : "Unknown"}</td>
             <td>{catData.breeds ? catData.breeds[0].name : "Unknown"}</td>
             <td>{catData.breeds ? catData.breeds[0].origin : "Unknown"}</td>
             <td>{catData.breeds ? catData.breeds[0].temperament : "Unknown"}</td>
+            <td>{catData.breeds ? catData.breeds[0].description : "No description available"}</td>
           </tr>
         </tbody>
       </table>
