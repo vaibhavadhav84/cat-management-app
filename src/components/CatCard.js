b3d2a6d0cd3725c14af7bbb602d7a99d15f5c691
Vaibhav Gajendra Adhav <154625347+vaibhavadhav84@users.noreply.github.com>
2025-01-25 21:49:52 +0530

    Update CatCard.js

--- src/components/CatCard.js
+++ src/components/CatCard.js
@@ -6,7 +6,9 @@ const CatCard = ({ catData }) => {
 
       <img src={catData.url} alt="Cat" className="cat-image-large" />
 
+      
       <h2>{catData.breeds ? catData.breeds[0].name : "Unknown"}</h2>
+      <p><strong>ID:</strong> {catData.breeds ? catData.breeds[0].id: "Unknown"}</p>
       <p><strong>Origin:</strong> {catData.breeds ? catData.breeds[0].origin : "Unknown"}</p>
       <p><strong>Temperament:</strong> {catData.breeds ? catData.breeds[0].temperament : "Unknown"}</p>
       <p><strong>Description:</strong> {catData.breeds ? catData.breeds[0].description : "No description available"}</p>
